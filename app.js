6c3a34b30097b6754550f7ab718408a855a58741
giridhar30 <giriit30@gmail.com>
2022-04-27 13:04:57 +0530

    completed config+create&delete completed

--- app.js
+++ app.js
@@ -3,20 +3,58 @@ const app = express();
 
 require('dotenv/config');
 
-const db = require('./models/db-config');
+const db = require('./orm/db-config');
 db.sequelize.sync();
+
+const knex = require('./qb/db-config');
 
 app.use(express.json());
 
 app.get('/', (req, res) => res.send("working!"));
 
-app.get('/employee', (req, res) => {
+app.get('/orm/employee', (req, res) => {
     db.employee.findAll().then(data => res.json(data));
 });
 
-app.delete('/dropEmployeeTable', (req, res) => {
+app.delete('/orm/dropTables', (req, res) => {
     db.employee.drop();
+    db.team.drop();
     res.send("dropped");
-})
+});
+
+app.get('/qb/createTables', async (req, res) => {
+
+    try {
+        await knex.schema.createTable('team_qb', table => {
+            console.log(table);
+            table.string('tname');
+            table.increments('id').primary();
+        })
+        
+        await knex.schema.createTable('employee_qb', table => {
+            table.string('ename');
+            table.increments('id').primary();
+            table.string('email');
+            table.string('designation');
+            table.integer('salary');
+            // table.foreign('id').references('id').inTable('team_qb');
+            table.integer('team_id')
+                .unsigned()
+                .index()
+                .references('id')
+                .inTable('team_qb');
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.send("created tables");
+});
+
+app.delete('/qb/dropTables', async (req, res) => {
+    await knex.schema.dropTableIfExists('employee_qb');
+    await knex.schema.dropTableIfExists('team_qb');
+    res.send("dropped tables");
+});
 
 app.listen(process.env.PORT, () => console.log(`app running on port ${process.env.PORT}`));
---
+++ qb/employee.model.js
@@ -0,0 +1,15 @@
+class Employee {
+    eid;
+    ename;
+    designation;
+    salary;
+    email;
+
+    constructor(eid, ename, email, designation, salary) {
+        this.eid = eid;
+        this.ename = ename;
+        this.designation = designation;
+        this.email = email;
+        this.salary = salary;
+    }
+}
