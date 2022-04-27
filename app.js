f10291e0e4e4c0ebdde5e3c0629a236a9e92fa35
YADLAPALLI VENKATA LALITH KUMAR <yvlalithkumar@gmail.com>
2022-04-27 17:17:26 +0530

    get by id and sum

--- app.js
+++ app.js
@@ -57,4 +57,74 @@ app.delete('/qb/dropTables', async (req, res) => {
     res.send("dropped tables");
 });
 
+
+// Select record by Id 
+    // Using ORM (sequelize)
+    app.get('/orm/employee/:eid', async(req, res) => {
+            const employee = await db.employee.findByPk(req.params.eid);
+            
+            if(employee === null){
+                res.send("Employee not found with given id");            
+            } else {
+                res.status(200).json(employee);
+            }   
+    })
+    app.get('/orm/team/:tid', async(req, res) => {
+            const team = await db.team.findByPk(req.params.tid);
+            
+            if(team === null){
+                res.send("Team not found with given id");            
+            } else {
+                res.status(200).json(team);
+            }   
+    })
+
+    // Using QueryBuilder (knex)
+    app.get('/qb/employee/:eid', async(req, res) => {
+        const employee = await knex.from('employee_qb').where({
+            id: req.params.eid
+        }).first()
+        console.log(employee);
+        
+        if(!employee){
+            res.send("Employee not found with given id");            
+        } else {
+            res.status(200).json(employee);
+        }   
+    })
+
+    app.get('/qb/team/:tid', async(req, res) => {
+        const team = await knex.from('team_qb').where({
+            id: req.params.tid
+        }).first()
+
+        if(!team){
+            res.send("Team not found with given id");            
+        } else {
+            res.status(200).json(team);
+        }   
+    })
+
+// Aggregate functions
+    // Using ORM (sequelize)
+    app.get('/orm/employeeSalarySum', async(req, res) => {
+        const salary = await db.employee.sum('salary'); 
+        
+        if(salary === null){
+            res.send("Sum not got");            
+        } else {
+            res.status(200).json(salary);
+        }   
+    })
+    // Using QueryBuilder (knex)
+    app.get('/qb/employeeSalarySum', async(req, res) => {
+        const salary = await knex.from('employee_qb').sum('salary')
+
+        if(!salary){
+            res.send("Team not found with given id");            
+        } else {
+            res.status(200).json(salary[0]["sum(`salary`)"]);
+        }   
+    })
+
 app.listen(process.env.PORT, () => console.log(`app running on port ${process.env.PORT}`));
